e6a454d9e5bfeb32808e531f198475bbdfe9c232
weilun <weilun@staff.weibo.com>
2016-03-06 20:29:02 +0800

    init the channel client

--- Applications/YourApp/start_businessworker.php
+++ Applications/YourApp/start_businessworker.php
@@ -19,6 +19,8 @@ use \Workerman\Autoloader;
 
 // 自动加载类
 require_once __DIR__ . '/../../Workerman/Autoloader.php';
+require_once __DIR__ . '/../../Channel/src/Client.php';
+
 Autoloader::setRootPath(__DIR__);
 
 
@@ -29,6 +31,17 @@ $worker->name = 'YourAppBusinessWorker';
 // bussinessWorker进程数量
 $worker->count = 4;
 
+$worker->onWorkerStart = function(){
+    Channel\Client::connect();
+    // 订阅broadcast事件，并注册事件回调
+    Channel\Client::on('user_login', function($event_data)use($worker){
+        // 向所当前worker进程的所有客户端广播消息
+        foreach($worker->connections as $connection)
+        {
+            $connection->send($event_data);
+        }
+    });
+};
 
 // 如果不是在根目录启动，则运行runAll方法
 if(!defined('GLOBAL_START'))
--- Applications/YourApp/start_websocket.php
+++ Applications/YourApp/start_websocket.php
@@ -2,7 +2,20 @@
 
 use \Workerman\Worker;
 
-$websocket_worker = new Worker('Websocket://0.0.0.0:3001');
-$websocket_worker->onConnect = function($connection){
+require_once __DIR__ . '/../../Channel/src/Client.php';
+
+$ws_worker = new Worker('Websocket://0.0.0.0:3001');
+
+$ws_worker->onWorkerStart = function() {
+    Channel\Client::connect();
+};
+
+$ws_worker->onConnect = function($connection){
     CustomLogger::getLogger()->info("WebSocket A client conenct");
+    CustomLogger::getLogger()->info($connection->id);
+
+    $event_name = "user_login";
+    $event_data = array('uid' => 123, 'uname' => 'tom');
+
+    Channel\Client::publish($event_name, $event_data);
 };
